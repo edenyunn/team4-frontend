c60e044bacd22237ee0a7468ec38a43f917ef730
edenyunn <dongyunlee617@gmail.com>
2024-10-17 17:06:08 +0900

    타임라인 초안(베트남전쟁까지)

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,13 +1,498 @@
 <template>
-  hi!!!!!
+  <div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1909 안중근 의거</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>영웅</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20221213_158%2F1670910727328mpqYu_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+  
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1919 3.1운동</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>항거: 유관순 이야기</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190207_119%2F1549515732449nv5tM_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1920 봉오동전투 </h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>봉오동전투</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190719_213%2F1563513194134ksAGD_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+  <div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1923 황옥 경부 폭탄사건</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>밀정</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20160823_114%2F1471936485192cjjcD_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1923 박열 재판</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>박열</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20170607_7%2F1496812951145IU2Bn_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1931 만주사변</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>덕혜옹주</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20160805_159%2F1470378561223pYzbp_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1932 우가키 가즈시게 암살 작전</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>암살</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20150713_74%2F14367488229802tA3J_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1939 중일전쟁</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>마이웨이</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111224_109%2F13246554789677iLlv_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1942 조선어학회 사건</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>말모이</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181227_80%2F1545901137289EGbWK_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1944 윤동주 형무소 수감</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>동주</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20160205_299%2F1454651273096sYmsK_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1945 일본의 패망과 광복</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>군함도</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20170726_24%2F1501035701931zYdMD_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1947 보스턴 마라톤 우승</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>1947 보스톤</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20230927_32%2F1695790185763mU0aR_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.6.25</h3>
+    <!--<p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>-->
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <!--<h3>태극기 휘날리며</h3>-->
+    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA2MjNfNTMg%2FMDAxNjg3NDk2OTc3Mjc4.ody0fhzY9L36FTMsXcONEqUdJgvQJyWvJHmhXmL1Rysg.54jDafAYKCwe53Ha-zD8LBTPrk3L99GshNhQIMDTxswg.PNG.gagijong%2F20230621_111246.png&type=a340" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.08 다부동 전투</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>태극기 휘날리며</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240516_197%2F1715836935649XMTDd_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.08 포항 전투</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>포화 속으로</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111223_44%2F1324618561837t2XF2_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.09 장사 상륙 작전</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>장사리: 잊혀진 영웅들</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190821_138%2F1566362458427V6Usp_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.09 인천상륙작전</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>인천상륙작전</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20160715_16%2F1468568203431zYbBs_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1950.11 중공군 개입</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>웰컴투 동막골</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111222_184%2F1324548976401Dstma_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1953 휴전 협정</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>고지전</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111223_104%2F1324638521898Mtz5U_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1960-61 4.19혁명/5.16군사정변</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>효자동 이발사</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111222_266%2F1324558463907JwOy7_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1963 서독 광부 파견</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>국제시장</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20141124_107%2F141679124450580TTS_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1968 1.21사태</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>실미도</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111222_50%2F1324503591769RwYSb_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1971 대한한공 F27기 납북미수사건</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>하이재킹</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240705_147%2F1720142916385Hwze0_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1971 제7대 대통령 사건</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>킹메이커</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20220126_227%2F1643182432127j8nuq_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1971 베트남 전쟁</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>님은 먼 곳에</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20120110_268%2F1326173819238C3QDb_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
+<div class="timeline-item">
+  <!-- 왼쪽: 사건 년도 및 설명 -->
+  <div class="timeline-content left">
+    <h3>1972 베트남 전쟁</h3>
+    <p>그는 이중스파이일까, 아니면 진정한 애국자일까?</p>
+  </div>
+  <!-- 타임라인 중앙의 날짜 -->
+  <div class="timeline-date"></div>
+  <!-- 오른쪽: 영화 제목과 포스터 -->
+  <div class="timeline-content right">
+    <h3>알포인트</h3>
+    <img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111223_224%2F1324566944253ErNPq_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="밀정 포스터" class="movie-poster">
+  </div>
+</div>
+
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+/* 기본 타임라인 스타일 */
+.timeline {
+  position: relative;
+  margin: 20px auto;
+  padding: 20px 0;
+  width: 90%;
+  max-width: 1200px;
+  box-sizing: border-box;
+}
 
+.timeline::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  width: 2px;
+  background-color: #ddd;
+}
+
+.timeline-item {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 50px;
+}
+
+.timeline-content {
+  position: relative;
+  width: 45%;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.timeline-date {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #461c75;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 10px;
+  border-radius: 50%;
+  z-index: 1;
+}
+
+/* 왼쪽 배치 */
+.timeline-content.left {
+  order: 1;
+  text-align: right;
+}
+
+/* 오른쪽 배치 */
+.timeline-content.right {
+  order: 2;
+  text-align: left;
+}
+
+.timeline-item:nth-child(even) .timeline-date {
+  left: auto;
+  right: 50%;
+  transform: translate(50%, -50%);
+}
+
+/* 반응형 스타일 */
+@media screen and (max-width: 768px) {
+  .timeline {
+    width: 100%;
+  }
+
+  .timeline-item {
+    flex-direction: column;
+  }
+
+  .timeline-content {
+    width: 90%;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .timeline-content.left,
+  .timeline-content.right {
+    order: 1;
+    text-align: center;
+  }
+
+  .timeline-date {
+    position: relative;
+    margin-bottom: 20px;
+    left: 0;
+    right: 0;
+    transform: none;
+  }
+}
 </style>

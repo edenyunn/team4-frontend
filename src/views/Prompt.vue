e36b6ac5e527fcf3341b6ad75eb7211b709a7483
neasocorpros <seungminkangdev@gmail.com>
2024-10-24 13:42:31 +0900

    llm과 prompt.vue 연결

--- src/views/Prompt.vue
+++ src/views/Prompt.vue
@@ -1,66 +1,43 @@
-<!-- components/UserPrompt.vue -->
 <template>
-    <div id="userPrompt">
-      <!-- 채팅 로그 보여주는 곳 -->
-      <section class="chatbox bg-body-secondary p-3 rounded-2">
-        <!-- v-for 로 반복을 할 때는 => :key="" 이 있어야 한다. -->
-        <article v-for="(chat, idx) in chatHistory" :key="idx"
-          class="mb-3 d-flex" :class="{ 'justify-content-end': chat.isUser }">
-  
-          <div class="card mw-6">
-            <div class="card-body">
-              {{ chat.message }}
-            </div>
-          </div>
-  
-        </article>
-  
-      </section>
-      <hr>
-      <!-- 프롬프트 입력하는 곳 -->
-      <div class="input-group">
-        <span class="input-group-text">Prompt</span>
-        <textarea v-model="userPrompt" class="form-control" aria-label="Prompt" rows="3"></textarea>
-        <button @click="onBtnClick" class="btn btn-primary">
-          <i class="bi bi-check-lg"></i>
-        </button>
-      </div>
-    </div>
-  </template>
-  <script>
-  import axios from 'axios'
-  const BASE_URL = 'https://jsonplaceholder.typicode.com/posts/'
-  
-  export default {
-    data() {
-      return {
-        chatHistory: [],
-        userPrompt: '',
-      }
-    },
-    methods: {
-      // async => 함수 안에 axios 라는 말이 있으면 쓴다.
-      async onBtnClick() {
-        // data 에 있는 값들은 this를 붙여야 한다!
-        this.chatHistory.push({ isUser: true, message: this.userPrompt })
-        // URL 로 요청을 보냄. [axios 앞에는 await가 필요하다
-        const res = await axios.get(BASE_URL + this.userPrompt)
-        // 받은 응답메시지를 chatHistory 추가함
-        this.chatHistory.push({ isUser: false, message: res.data.body })
-        // 사용자 입력 비우기
-        this.userPrompt = ''
-      }
+  <textarea rows="3" cols="40" v-model="userInput"></textarea>
+  <button @click="onBtnClick">전송</button>
+  <ul>
+    <li v-for="(chat, idx) in chatHistory" :key="idx">
+      <span v-if="chat.isUser">사용자: </span>
+      <span v-else>LLM: </span>
+      {{ chat.message }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      userInput: '',
+      chatHistory: [],
+    }
+  },
+  methods: {
+    async onBtnClick() {
+      // 사용자 메세지를 chatHistory 에 넣고
+      this.chatHistory.push({ isUser: true, message: this.userInput })
+      // 우리 서버로 요청을 보내고
+      const res = await axios.post('http://127.0.0.1:5000/', {
+        message: this.userInput,
+      })
+      // 응답 메세지도 넣는다.
+      const llmOutput = { isUser: false, message: res.data.llm }
+      this.chatHistory.push(llmOutput)
+      // 입력창 비워주기
+      this.userInput = ''
     }
   }
-  </script>
-  
-  <style>
-  .chatbox {
-    height: 50vh;
-    overflow: auto;
-  }
-  
-  .mw-6 {
-    max-width: 60%;
-  }
-  </style>
+}
+</script>
+
+<style>
+
+</style>

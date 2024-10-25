9e76ac278a7d2aa58a5bae9ae7454f06f674c5a2
edenyunn <dongyunlee617@gmail.com>
2024-10-25 16:40:56 +0900

    타임라인 사진 교체 완료

--- src/assets/movies.js
+++ src/assets/movies.js
@@ -5,30 +5,49 @@ import 밀정 from '@/assets/posters/밀정.jpg'
 import 박열 from '@/assets/posters/박열.jpg'
 import 덕혜옹주 from '@/assets/posters/덕혜옹주.jpg'
 import 암살 from '@/assets/posters/암살.jpg'
+import 마이웨이 from '@/assets/posters/마이웨이.jpg'
+import 말모이 from '@/assets/posters/말모이.jpg'
 import 동주 from '@/assets/posters/동주.jpg'
+import 군함도 from '@/assets/posters/군함도.jpg'
+import 보스톤 from '@/assets/posters/보스톤.jpg'
 import 태극기휘날리며 from '@/assets/posters/태극기휘날리며.jpg'
 import 포화속으로 from '@/assets/posters/포화속으로.jpg'
+import 장사리 from '@/assets/posters/장사리.jpg'
 import 인천상륙작전 from '@/assets/posters/인천상륙작전.jpg'
+import 웰컴투동막골 from '@/assets/posters/웰컴투동막골.jpg'
 import 고지전 from '@/assets/posters/고지전.jpg'
 import 효자동이발사 from '@/assets/posters/효자동이발사.jpg'
 import 국제시장 from '@/assets/posters/국제시장.jpg'
 import 실미도 from '@/assets/posters/실미도.jpg'
 import 킹메이커 from '@/assets/posters/킹메이커.jpg'
+import 하이재킹 from '@/assets/posters/하이재킹.jpg'
+import 님은먼곳에 from '@/assets/posters/님은먼곳에.jpg'
+import 알포인트 from '@/assets/posters/알포인트.jpg'
 import 남산의부장들 from '@/assets/posters/남산의부장들.jpg'
 import 그때그사람들 from '@/assets/posters/그때그사람들.jpg'
+import 행복의나라 from '@/assets/posters/행복의나라.jpg'
 import 서울의봄 from '@/assets/posters/서울의봄.jpg'
 import 화려한휴가 from '@/assets/posters/화려한휴가.jpg'
 import 택시운전사 from '@/assets/posters/택시운전사.jpg'
+import 박하사탕 from '@/assets/posters/박하사탕.jpg'
 import 변호인 from '@/assets/posters/변호인.jpg'
 import 헌트 from '@/assets/posters/헌트.jpg'
+import 남영동 from '@/assets/posters/남영동.png'
+import 비공식작전 from '@/assets/posters/비공식작전.jpg'
+import 살인의추억 from '@/assets/posters/살인의추억.jpg'
 import 일구팔칠 from '@/assets/posters/1987.jpg'
 import 범죄와의전쟁 from '@/assets/posters/범죄와의전쟁.jpg'
+import 모가디슈 from '@/assets/posters/모가디슈.jpg'
+import 삼진그룹 from '@/assets/posters/삼진그룹.jpg'
+import 벌새 from '@/assets/posters/벌새.jpg'
 import 국가부도의날 from '@/assets/posters/국가부도의날.jpg'
 import 공작 from '@/assets/posters/공작.jpg'
 import 연평해전 from '@/assets/posters/연평해전.jpg'
+import 추격자 from '@/assets/posters/추격자.jpg'
 import 더킹 from '@/assets/posters/더킹.jpg'
 import 아이캔스피크 from '@/assets/posters/아이캔스피크.jpg'
 import 댓글부대 from '@/assets/posters/댓글부대.jpg'
+import 내부자들 from '@/assets/posters/내부자들.jpg'
 
 export default [
     { id: 1, title: '', eventYear: 1909, event: '안중근 의거', summary: '한 발의 총성으로, 역사가 흔들렸다', imageUrl: 영웅 },
@@ -37,48 +56,48 @@ export default [
     { id: 4, title: '', eventYear: 1923, event: '황옥 경부 폭탄사건', summary: '이중 스파이일까, 아니면 진정한 애국자일까?', imageUrl: 밀정 },
     { id: 5, title: '', eventYear: 1923, event: '박열 재판', summary: '조선의 미친놈, 일본 법정을 뒤집다', imageUrl: 박열 },
     { id: 6, title: '', eventYear: 1931, event: '만주사변', summary: '', imageUrl: 덕혜옹주 },
-    { id: 7, title: '', eventYear: 1932, event: '우가키 조선총독 암살 작전', summary: '목표는 하나, 조국을 위한 암살', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20150713_74%2F14367488229802tA3J_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 8, title: '', eventYear: 1937, event: '중일전쟁', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111224_109%2F13246554789677iLlv_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 9, title: '', eventYear: 1942, event: '조선어학회 사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181227_80%2F1545901137289EGbWK_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 7, title: '', eventYear: 1932, event: '우가키 조선총독 암살 작전', summary: '목표는 하나, 조국을 위한 암살', imageUrl: 암살 },
+    { id: 8, title: '', eventYear: 1937, event: '중일전쟁', summary: '', imageUrl: 마이웨이 },
+    { id: 9, title: '', eventYear: 1942, event: '조선어학회 사건', summary: '', imageUrl: 말모이 },
     { id: 10, title: '', eventYear: 1944, event: '윤동주 형무소 수감', summary: '짧았던 청춘, 영원한 시', imageUrl: 동주 },
-    { id: 11, title: '', eventYear: 1945, event: '일본의 패망과 광복', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20170726_24%2F1501035701931zYdMD_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 12, title: '', eventYear: 1947, event: '보스턴 마라톤 우승', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20230927_32%2F1695790185763mU0aR_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 11, title: '', eventYear: 1945, event: '일본의 패망과 광복', summary: '', imageUrl: 군함도 },
+    { id: 12, title: '', eventYear: 1947, event: '보스턴 마라톤 우승', summary: '', imageUrl: 보스톤 },
     { id: 13, title: '', eventYear: 1950.08, event: '다부동 전투', summary: '돌아온다고 약속했잖아요', imageUrl: 태극기휘날리며 },
     { id: 14, title: '', eventYear: 1950.08, event: '포항 전투', summary: '포화 소리에 묻힌 그들의 청춘', imageUrl: 포화속으로 },
-    { id: 15, title: '', eventYear: 1950.09, event: '장사상륙작전', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190821_138%2F1566362458427V6Usp_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 15, title: '', eventYear: 1950.09, event: '장사상륙작전', summary: '', imageUrl: 장사리 },
     { id: 16, title: '', eventYear: 1950.09, event: '인천상륙작전', summary: '기적? 그건 작전이었다', imageUrl: 인천상륙작전 },
-    { id: 17, title: '', eventYear: 1950.11, event: '중공군 개입', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111222_184%2F1324548976401Dstma_JPEG%3Ftype%3Dw640_2' },
+    { id: 17, title: '', eventYear: 1950.11, event: '중공군 개입', summary: '', imageUrl: 웰컴투동막골 },
     { id: 18, title: '', eventYear: 1953, event: '휴전 협정', summary: '산은 기억한다, 헛되이 흘린 젊은 피를', imageUrl: 고지전 },
     { id: 19, title: '', eventYear: 1960, event: '4·19혁명', summary: '‘대머리 그 남자’의 은밀한 헤어컷', imageUrl: 효자동이발사 },
     { id: 20, title: '', eventYear: 1963, event: '서독 광부 파견', summary: 'K-아버지 주름살', imageUrl: 국제시장 },
     { id: 21, title: '', eventYear: 1968, event: '1·21사태', summary: '국가가 버린 31명의 살인 병기', imageUrl: 실미도 },
-    { id: 22, title: '', eventYear: 1971, event: '대한한공 F27기 납북미수사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240705_147%2F1720142916385Hwze0_JPEG%3Ftype%3Dw640_2' },
+    { id: 22, title: '', eventYear: 1971, event: '대한한공 F27기 납북미수사건', summary: '', imageUrl: 하이재킹 },
     { id: 23, title: '', eventYear: 1971, event: '제7대 대통령 선거', summary: '정치판 서커스, 진짜 설계자는 따로 있다', imageUrl: 킹메이커 },
-    { id: 24, title: '', eventYear: 1971, event: '베트남 전쟁', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20120110_268%2F1326173819238C3QDb_JPEG%3Ftype%3Dw640_2' },
-    { id: 25, title: '', eventYear: 1972, event: '베트남 전쟁', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111223_224%2F1324566944253ErNPq_JPEG%3Ftype%3Dw640_2' },
+    { id: 24, title: '', eventYear: 1971, event: '베트남 전쟁', summary: '', imageUrl: 님은먼곳에 },
+    { id: 25, title: '', eventYear: 1972, event: '베트남 전쟁', summary: '', imageUrl: 알포인트 },
     { id: 26, title: '', eventYear: 1979, event: '10·26사태', summary: '당신의 저녁 식사가 정치적 음모의 무대가 된다면?', imageUrl: 남산의부장들 },
     { id: 27, title: '', eventYear: 1979, event: '10·26사태', summary: '남산에 계신 부장님들보다는 훨씬 유머러스하네요', imageUrl: 그때그사람들 },
-    { id: 28, title: '', eventYear: 1979, event: '12·12사태', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240816_267%2F1723785813255Qbf1b_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 28, title: '', eventYear: 1979, event: '12·12사태', summary: '', imageUrl: 행복의나라 },
     { id: 29, title: '', eventYear: 1979, event: '12·12사태', summary: '1979년 12월, 누구도 예상하지 못한 생일잔치', imageUrl: 서울의봄 },
     { id: 30, title: '', eventYear: 1980, event: '5·18민주화운동', summary: '전혀 화려하지 않았던 휴가', imageUrl: 화려한휴가 },
     { id: 31, title: '', eventYear: 1980, event: '5·18민주화운동', summary: '노 광주, 노 민주(주의)', imageUrl: 택시운전사 },
-    { id: 32, title: '', eventYear: 1980, event: '5·18민주화운동', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20180416_198%2F1523869200043wycgA_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 32, title: '', eventYear: 1980, event: '5·18민주화운동', summary: '', imageUrl: 박하사탕 },
     { id: 33, title: '', eventYear: 1981, event: '부림 사건', summary: '‘돼지국밥’처럼 뜨거운 변호인의 인권 투쟁', imageUrl: 변호인 },
     { id: 34, title: '', eventYear: 1983, event: '아웅 산 묘소 폭탄 테러 사건', summary: '코리아 서스펜스: 적인가, 동료인가?', imageUrl: 헌트 },
-    { id: 35, title: '', eventYear: 1985, event: '서울대 민추위 사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20121105_185%2F1352082254341CgCHX_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 36, title: '', eventYear: 1986, event: '레바논 한국 외교관 납치 사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20230802_198%2F1690967873095A7TxE_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 37, title: '', eventYear: 1986, event: '화성연쇄살인사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111222_166%2F1324548172003mpyIa_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 35, title: '', eventYear: 1985, event: '서울대 민추위 사건', summary: '', imageUrl: 남영동 },
+    { id: 36, title: '', eventYear: 1986, event: '레바논 한국 외교관 납치 사건', summary: '', imageUrl: 비공식작전 },
+    { id: 37, title: '', eventYear: 1986, event: '화성연쇄살인사건', summary: '', imageUrl: 살인의추억 },
     { id: 38, title: '', eventYear: 1987, event: '6월민주항쟁', summary: '민주주의까지 남은 거리, 87m', imageUrl: 일구팔칠 },
     { id: 39, title: '', eventYear: 1990, event: '10·13 특별선언', summary: '’느그 서장’이랑 친하면 다 되는 세상', imageUrl: 범죄와의전쟁 },
-    { id: 40, title: '', eventYear: 1991, event: '소말리아 외교공관 철수', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20210820_122%2F1629448997924YXUCv_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 41, title: '', eventYear: 1991, event: '낙동강 페놀 유출사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20201021_273%2F16032618054109Yvje_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 42, title: '', eventYear: 1994, event: '성수대교 붕괴', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190805_121%2F1564969695835Iq9Qc_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 43, title: '', eventYear: 1997, event: 'IMF 외환위기', summary: '그날 우리 아버지가 해고 당한 이유', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20181105_232%2F1541392865465aVb5V_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 44, title: '', eventYear: 1997, event: '제15대 대통령 선거', summary: '서로를 믿고 싶었던 두 남자의 ‘비즈니스’', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20180730_299%2F1532914028702Mgwti_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 45, title: '', eventYear: 2002, event: '제2연평해전', summary: '모두가 붉게 물든 2002년, 진짜 영웅은 따로 있었다', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20150709_22%2F1436403002631h77Nm_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
-    { id: 46, title: '', eventYear: 2003, event: '유영철 연쇄살인 사건', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20111223_156%2F1324566721937R1fPE_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 40, title: '', eventYear: 1991, event: '소말리아 외교공관 철수', summary: '', imageUrl: 모가디슈 },
+    { id: 41, title: '', eventYear: 1991, event: '낙동강 페놀 유출사건', summary: '', imageUrl: 삼진그룹 },
+    { id: 42, title: '', eventYear: 1994, event: '성수대교 붕괴', summary: '', imageUrl: 벌새 },
+    { id: 43, title: '', eventYear: 1997, event: 'IMF 외환위기', summary: '그날 우리 아버지가 해고 당한 이유', imageUrl: 국가부도의날 },
+    { id: 44, title: '', eventYear: 1997, event: '제15대 대통령 선거', summary: '서로를 믿고 싶었던 두 남자의 ‘비즈니스’', imageUrl: 공작 },
+    { id: 45, title: '', eventYear: 2002, event: '제2연평해전', summary: '모두가 붉게 물든 2002년, 진짜 영웅은 따로 있었다', imageUrl: 연평해전 },
+    { id: 46, title: '', eventYear: 2003, event: '유영철 연쇄살인 사건', summary: '', imageUrl: 추격자 },
     { id: 47, title: '', eventYear: 2004, event: '노무현 탄핵 소추', summary: '대한민국 현대 정치사 입문서', imageUrl: 더킹 },
     { id: 48, title: '', eventYear: 2007, event: '미국 의회 위안부 결의안', summary: '할머니의 사연 있는 ‘ABCD’', imageUrl: 아이캔스피크 },
     { id: 49, title: '', eventYear: 2012, event: '국정원·국방부 여론조작 의혹', summary: '클릭 한 번으로 세상을 조작할 수 있다면', imageUrl: 댓글부대 },
-    { id: 50, title: '', eventYear: 2016, event: '고위 공무원 국민 개·돼지 발언', summary: '', imageUrl: 'https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20151021_172%2F14453964781624mCqX_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2' },
+    { id: 50, title: '', eventYear: 2016, event: '고위 공무원 국민 개·돼지 발언', summary: '', imageUrl: 내부자들 },
 ]

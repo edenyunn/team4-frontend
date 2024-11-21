3371331b4a7b1302f5daad14232e2ccf95d49dac
neasocorpros <seungminkangdev@gmail.com>
2024-11-21 14:06:11 +0900

    오리지널 최종본 업데이트

--- src/views/Original.vue
+++ src/views/Original.vue
@@ -13,7 +13,7 @@
     <!-- Related Videos Section -->
     <div class="video-container">
       <div class="iframe-wrapper">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/6P72Wcdvt7U?si=s7J9VkGqlPNALgAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jJxbFM0VLyA?si=t9OHH4ebjRqnq8m0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     </div>
 
